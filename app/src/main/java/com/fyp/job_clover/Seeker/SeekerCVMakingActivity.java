ccaae9143e378ffeb0470cf3ba61cb38d5085713
Hafiz Ghiyas <bilalt586@gmail.com>
2020-07-03 21:40:15 +0500

    new_one

--- app/src/main/java/com/fyp/job_clover/Seeker/SeekerCVMakingActivity.java
+++ app/src/main/java/com/fyp/job_clover/Seeker/SeekerCVMakingActivity.java
@@ -4,8 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -30,13 +34,20 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.skydoves.elasticviews.ElasticButton;
 import com.ybs.countrypicker.CountryPicker;
 import com.ybs.countrypicker.CountryPickerListener;
 
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class SeekerCVMakingActivity extends AppCompatActivity {
+    private static final int STORAGE_CODE = 1000;
     private EditText skills,exper,phone;
     private TextInputEditText title,name,email,city,address;
     private String cv_job_title,cv_name,cv_email,cv_phone,cv_p_code,cv_req_education,cv_city,cv_address,
@@ -87,87 +98,142 @@ public class SeekerCVMakingActivity extends AppCompatActivity {
         if (TextUtils.isEmpty(title.getText().toString())){
             Toast.makeText(getApplicationContext(), "Job Title is required", Toast.LENGTH_SHORT).show();
         }
-        else if (TextUtils.isEmpty(name.getText().toString())) {
-            Toast.makeText(getApplicationContext(), " Name is required", Toast.LENGTH_SHORT).show();
-
-        }
-        else if (TextUtils.isEmpty(email.getText().toString())){
-            Toast.makeText(getApplicationContext(), "Email is required", Toast.LENGTH_SHORT).show();
-        }
-        else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
-            email.setError("Email is not Valid");
-            email.requestFocus();
-        }
-        else if (TextUtils.isEmpty(city.getText().toString())){
-            Toast.makeText(getApplicationContext(), "City is required", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(address.getText().toString())){
-            Toast.makeText(getApplicationContext(), "Address is required", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(phoneCode.getText().toString())){
-            Toast.makeText(getApplicationContext(), "phone code is required", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(phone.getText().toString())){
-            Toast.makeText(getApplicationContext(), " phone is required", Toast.LENGTH_SHORT).show();
-        }
-        else if (edu == null){
-            Toast.makeText(getApplicationContext(), "Education is required", Toast.LENGTH_SHORT).show();
-            educat.requestFocus();
-        }
-
-        else if (TextUtils.isEmpty(skills.getText().toString())){
-            Toast.makeText(getApplicationContext(), "Skills is  required", Toast.LENGTH_SHORT).show();
-        }
-        else if (TextUtils.isEmpty(exper.getText().toString())){
-            Toast.makeText(getApplicationContext(), "Experience  is required", Toast.LENGTH_SHORT).show();
-        }
+//        else if (TextUtils.isEmpty(name.getText().toString())) {
+//            Toast.makeText(getApplicationContext(), " Name is required", Toast.LENGTH_SHORT).show();
+//
+//        }
+//        else if (TextUtils.isEmpty(email.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "Email is required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()){
+//            email.setError("Email is not Valid");
+//            email.requestFocus();
+//        }
+//        else if (TextUtils.isEmpty(city.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "City is required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (TextUtils.isEmpty(address.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "Address is required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (TextUtils.isEmpty(phoneCode.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "phone code is required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (TextUtils.isEmpty(phone.getText().toString())){
+//            Toast.makeText(getApplicationContext(), " phone is required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (edu == null){
+//            Toast.makeText(getApplicationContext(), "Education is required", Toast.LENGTH_SHORT).show();
+//            educat.requestFocus();
+//        }
+//
+//        else if (TextUtils.isEmpty(skills.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "Skills is  required", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (TextUtils.isEmpty(exper.getText().toString())){
+//            Toast.makeText(getApplicationContext(), "Experience  is required", Toast.LENGTH_SHORT).show();
+//        }
 
         else {
             cv_job_title = title.getText().toString();
-            cv_name = name.getText().toString();
-            cv_email = email.getText().toString();
-            cv_city = city.getText().toString();
-            cv_address = address.getText().toString();
-            cv_p_code = phoneCode.getText().toString();
-            cv_phone = phone.getText().toString();
-            cv_skills = skills.getText().toString();
-            cv_experience = exper.getText().toString();
-
-            String cv_contact = cv_p_code + cv_phone;
-
-
-            final SweetAlertDialog dialog = new SweetAlertDialog(getApplicationContext(),SweetAlertDialog.PROGRESS_TYPE);
-            dialog.setTitleText("Posting...");
-            dialog.setCancelable(false);
-            dialog.show();
+//            cv_name = name.getText().toString();
+//            cv_email = email.getText().toString();
+//            cv_city = city.getText().toString();
+//            cv_address = address.getText().toString();
+//            cv_p_code = phoneCode.getText().toString();
+//            cv_phone = phone.getText().toString();
+//            cv_skills = skills.getText().toString();
+//            cv_experience = exper.getText().toString();
+//
+//            String cv_contact = cv_p_code + cv_phone;
 
 
-            CV_Making_Data cv = new CV_Making_Data(cv_job_title,cv_name,cv_email,cv_contact,
-                    edu,cv_city,cv_address,cv_skills,cv_experience);
+//            final SweetAlertDialog dialog = new SweetAlertDialog(getApplicationContext(),SweetAlertDialog.PROGRESS_TYPE);
+//            dialog.setTitleText("Posting...");
+//            dialog.setCancelable(false);
+//            dialog.show();
 
-            reference.child(auth.getCurrentUser().getUid())
-                    .setValue(cv).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
 
-                    dialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
-                    dialog.setTitleText("CV Save Successfully");
-                    dialog.setConfirmText("OK");
-                    dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
-                        @Override
-                        public void onClick(SweetAlertDialog sweetAlertDialog) {
-                            dialog.dismiss();
-                        }
-                    });
+//            CV_Making_Data cv = new CV_Making_Data(cv_job_title,cv_name,cv_email,cv_contact,
+//                    edu,cv_city,cv_address,cv_skills,cv_experience);
+//
+//            reference.child(auth.getCurrentUser().getUid())
+//                    .setValue(cv).addOnCompleteListener(new OnCompleteListener<Void>() {
+//                @Override
+//                public void onComplete(@NonNull Task<Void> task) {
+//
+//                    dialog.changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+//                    dialog.setTitleText("CV Save Successfully");
+//                    dialog.setConfirmText("OK");
+//                    dialog.setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+//                        @Override
+//                        public void onClick(SweetAlertDialog sweetAlertDialog) {
+//                            dialog.dismiss();
+//                        }
+//                    });
+//                }
+//            });
+//
+
+            //   text convert to pdf file simple
+
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED){
+
+                    String[] permissions =  {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                    requestPermissions(permissions, STORAGE_CODE);
                 }
-            });
+                else {
+                    savePdf();
+                }
+            }
+            else {
+                savePdf();
+
+            }
 
 
         }
 
     }
 
+    private void savePdf()
+    {
 
+        Document document = new Document();
+        String mFileName = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis());
+        String filePath = Environment.getExternalStorageDirectory() + "/" + mFileName + ".pdf";
+
+        try {
+
+            PdfWriter.getInstance(document,new FileOutputStream(filePath));
+
+            document.addAuthor("Ghiyas");
+            document.add(new Paragraph(cv_job_title));
+
+            document.close();
+
+            Toast.makeText(this, mFileName +".pdf \n is save to \n" + filePath, Toast.LENGTH_SHORT).show();
+
+        }
+        catch (Exception e){
+
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+         switch (requestCode){
+             case STORAGE_CODE:
+                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                        savePdf();
+                 }
+                 else {
+                     Toast.makeText(this, "Permission Denied ....!", Toast.LENGTH_SHORT).show();
+                 }
+         }
+    }
 
     private void countrypickerVal()
     {
